5af911e23f4fe223317d4a2865f71cd4d30cff89
Nakamatsu Minori <minori.naka31@gmail.com>
2023-06-20 20:38:02 +0900

    [udate]kv,header ver.1

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <!-- ヘッダー -->
+    <header>
+        <ul class="header_ul">
+            <li>Menu01</li>
+            <li>Menu02</li>
+            <li>Menu03</li>
+            <li>Menu04</li>
+        </ul>
+        <section>
+            <div class="kv">
+                <img src="./assets/img/visual_1_pc.png" alt="kv" id="kv_img">
+                <img src="./assets/img/logo.png" alt="logo" class="logo">
+                <img src="./assets/img/visual_text_pc.png" alt="visual_text" id="kv_visual_text">
+            </div>
+            <h1>進化し続ける「街」アメリカンビレッジマガジン</h1>
+        </section>
+    </header>
+
 </body>
+
 </html>
