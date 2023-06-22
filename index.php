5a22fae145d9646c22e3f27113e431fbd4c9102a
Hitomi Yoshimoto <hitomi.yoshimoto21@gmail.com>
2023-06-22 11:56:31 +0900

    latestの部分修正完了！

--- index.php
+++ index.php
@@ -45,48 +45,48 @@
             </div>
 
             <div class="box">
-                <img src="./assets/img/post_img_1.png" alt="">
+                <img src="./assets/img/post_img_2.png" alt="">
                 <div class="description">
-                    <p>2018/5/20</p>
-                    <h3>おしゃれカフェありますよ</h3>
+                    <p>2018/5/19</p>
+                    <h3>あのネオンはいつ交換するのか？！観覧車の謎に迫る！</h3>
                 </div>
                 <span>READ MORE <p></p></span>
             </div>
 
             <div class="box">
-                <img src="./assets/img/post_img_1.png" alt="">
+                <img src="./assets/img/post_img_3.png" alt="">
                 <div class="description">
-                    <p>2018/5/20</p>
-                    <h3>おしゃれカフェありますよ</h3>
+                    <p>2018/5/18</p>
+                    <h3>ラソナの車内はこんなのよ</h3>
                 </div>
                 <span>READ MORE <p></p></span>
             </div>
 
             <div class="box">
-                <img src="./assets/img/post_img_1.png" alt="">
+                <img src="./assets/img/post_img_4.png" alt="">
                 <div class="description">
-                    <p>2018/5/20</p>
-                    <h3>おしゃれカフェありますよ</h3>
+                    <p>2018/5/27</p>
+                    <h3>お隣のアラハはハワイ？</h3>
                 </div>
                 <span>READ MORE <p></p></span>
             </div>
 
             <div class="box">
-                <img src="./assets/img/post_img_1.png" alt="">
+                <img src="./assets/img/post_img_5.png" alt="">
                 <div class="description">
-                    <p>2018/5/20</p>
-                    <h3>おしゃれカフェありますよ</h3>
+                    <p>2018/5/16</p>
+                    <h3>なぜテント？ラソナの人に聞いてみた</h3>
                 </div>
                 <span>READ MORE <p></p></span>
             </div>
             <div class="box">
-                <img src="./assets/img/post_img_1.png" alt="">
+                <img src="./assets/img/post_img_6.png" alt="">
                 <div class="description">
-                    <p>2018/5/20</p>
-                    <h3>おしゃれカフェありますよ</h3>
+                    <p>2018/5/15</p>
+                    <h3>ベイエリアおしゃれすぎる問題</h3>
                 </div>
                 <span>READ MORE <p id="line"></p></span>
-        </div>
+            </div>
     </section>
     <footer>
         <p>Copyright © Shinohara. ALL RIGHTS RESERVED.</p>
