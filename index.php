5bb4c0f42da41d6252a01ebbe93819d18550ef64
Nakamatsu Minori <minori.naka31@gmail.com>
2023-06-22 21:00:27 +0900

    [update]競合後の

--- index.php
+++ index.php
@@ -32,6 +32,9 @@
                 <h1 id="kv_h1_sp">進化し続ける「街」<br>アメリカンビレッジマガジン</h1>
             </section>
         </header>
+        </section>
+
+        </header>
 
         <section>
             <h2>Latest Articles</h2>
@@ -46,107 +49,51 @@
                 </div>
 
                 <div class="box">
-                    <img src="./assets/img/post_img_1.png" alt="">
+                    <img src="./assets/img/post_img_2.png" alt="">
                     <div class="description">
-                        <p>2018/5/20</p>
-                        <h3>おしゃれカフェありますよ</h3>
+                        <p>2018/5/19</p>
+                        <h3>あのネオンはいつ交換するのか？！観覧車の謎に迫る！</h3>
                     </div>
                     <span>READ MORE <p></p></span>
                 </div>
 
                 <div class="box">
-                    <img src="./assets/img/post_img_1.png" alt="">
+                    <img src="./assets/img/post_img_3.png" alt="">
                     <div class="description">
-                        <p>2018/5/20</p>
-                        <h3>おしゃれカフェありますよ</h3>
+                        <p>2018/5/18</p>
+                        <h3>ラソナの車内はこんなのよ</h3>
                     </div>
                     <span>READ MORE <p></p></span>
                 </div>
 
                 <div class="box">
-                    <img src="./assets/img/post_img_1.png" alt="">
+                    <img src="./assets/img/post_img_4.png" alt="">
                     <div class="description">
-                        <p>2018/5/20</p>
-                        <h3>おしゃれカフェありますよ</h3>
+                        <p>2018/5/27</p>
+                        <h3>お隣のアラハはハワイ？</h3>
                     </div>
                     <span>READ MORE <p></p></span>
                 </div>
 
                 <div class="box">
-                    <img src="./assets/img/post_img_1.png" alt="">
+                    <img src="./assets/img/post_img_5.png" alt="">
                     <div class="description">
-                        <p>2018/5/20</p>
-                        <h3>おしゃれカフェありますよ</h3>
+                        <p>2018/5/16</p>
+                        <h3>なぜテント？ラソナの人に聞いてみた</h3>
                     </div>
                     <span>READ MORE <p></p></span>
                 </div>
                 <div class="box">
-                    <img src="./assets/img/post_img_1.png" alt="">
+                    <img src="./assets/img/post_img_6.png" alt="">
                     <div class="description">
-                        <p>2018/5/20</p>
-                        <h3>おしゃれカフェありますよ</h3>
+                        <p>2018/5/15</p>
+                        <h3>ベイエリアおしゃれすぎる問題</h3>
                     </div>
                     <span>READ MORE <p id="line"></p></span>
                 </div>
+
         </section>
-    </header>
-
-    <section>
-        <h2>Latest Articles</h2>
-        <div class="container">
-            <div class="box">
-                <img src="./assets/img/post_img_1.png" alt="">
-                <div class="description">
-                    <p>2018/5/20</p>
-                    <h3>おしゃれカフェありますよ</h3>
-                </div>
-                <span>READ MORE <p></p></span>
-            </div>
-
-            <div class="box">
-                <img src="./assets/img/post_img_2.png" alt="">
-                <div class="description">
-                    <p>2018/5/19</p>
-                    <h3>あのネオンはいつ交換するのか？！観覧車の謎に迫る！</h3>
-                </div>
-                <span>READ MORE <p></p></span>
-            </div>
-
-            <div class="box">
-                <img src="./assets/img/post_img_3.png" alt="">
-                <div class="description">
-                    <p>2018/5/18</p>
-                    <h3>ラソナの車内はこんなのよ</h3>
-                </div>
-                <span>READ MORE <p></p></span>
-            </div>
-
-            <div class="box">
-                <img src="./assets/img/post_img_4.png" alt="">
-                <div class="description">
-                    <p>2018/5/27</p>
-                    <h3>お隣のアラハはハワイ？</h3>
-                </div>
-                <span>READ MORE <p></p></span>
-            </div>
-
-            <div class="box">
-                <img src="./assets/img/post_img_5.png" alt="">
-                <div class="description">
-                    <p>2018/5/16</p>
-                    <h3>なぜテント？ラソナの人に聞いてみた</h3>
-                </div>
-                <span>READ MORE <p></p></span>
-            </div>
-            <div class="box">
-                <img src="./assets/img/post_img_6.png" alt="">
-                <div class="description">
-                    <p>2018/5/15</p>
-                    <h3>ベイエリアおしゃれすぎる問題</h3>
-                </div>
-                <span>READ MORE <p id="line"></p></span>
-            </div>
-    </section>
+    </div>
     <footer>
         <p>Copyright © Shinohara. ALL RIGHTS RESERVED.</p>
     </footer>
